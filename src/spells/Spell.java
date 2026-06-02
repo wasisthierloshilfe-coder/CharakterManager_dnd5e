74576f8cc387f5b15574619f74a2befbafb45206
Alexander Kalaschnikow <AKalaschnikow@ctc-lohr.net>
2026-06-02 08:32:53 +0200

    Setter and Getter für Spells

--- src/spells/Spell.java
+++ src/spells/Spell.java
@@ -1,12 +1,51 @@
 package spells;
 public class Spell{
-    String name;
-    String wirkzeit;
-    int reichweite;
-    String komponente;
-    int dauer;
+    private String name;
+    private String wirkzeit;
+    private int reichweite;
+    private String komponente;
+    private int dauer; 
+    private String beschreibung;
+
+    //Setter Methoden
+    public void setName(String name){
+        this.name =name;
+    }
+    public void setWirkzeit(String wirkzeit){
+        this.wirkzeit = wirkzeit;
+    }
+    public void setReichweite(int reichweite){
+        this.reichweite = reichweite;
+    }
+    public void setKomponente(String komponente){
+        this.komponente = komponente;
+    }
+    public void setDauer(int dauer){
+        this.dauer = dauer;
+    }
+    public void setBeschreibung(String beschreibung){
+        this.beschreibung = beschreibung;
+    }
+
+    //Getter Methoden
+    public String getName(){
+        return this.name;
+    }
+    public String getWirkzeit(){
+        return this.wirkzeit;
+    }
+    public int getReichweite(){
+        return this.reichweite;
+    }
+    public String getKomponente(){
+        return this.komponente;
+    }
+    public int getDauer(){
+        return this.dauer;
+    }
+    public String getBeschreibung(){
+        return this.beschreibung;
+    }
+
     
-    String beschreibung;
-
-
 }
