13f4a6c6e4cb5d7ddb719315a63e23918ac7d00d
Fargus <wasisthierloshilfe@web.de>
2026-06-22 15:14:07 +0200

    Effekte erstellt und Effekt implementiert

---
+++ src/effekte/FertigkeitEffekt.java
@@ -0,0 +1,52 @@
+package effekte;
+
+public class FertigkeitEffekt implements Effekt{
+    String quelle;
+    EffektTyp effektTyp;
+    int modifikator;
+    String beschreibung;
+
+    
+    //Setter
+    @Override
+    public void setQuelle(String quelle){
+        this.quelle = quelle;
+    }
+    @Override
+    public void setEffektTyp(EffektTyp typ){
+        this.effektTyp = typ;
+    }
+    @Override
+    public void setModifikator(int modifikator){
+        this.modifikator = modifikator;
+    }
+    @Override
+    public void setBeschreibung(String beschreibung){
+        this.beschreibung = beschreibung;
+    }
+
+
+//Getter
+    @Override
+    public String getQuelle() {
+        return this.quelle;
+    }
+    @Override
+    public EffektTyp getEffektTyp() {
+        return this.effektTyp;
+    }
+    @Override
+    public int getModifikator() {
+        return this.modifikator;
+    }
+    @Override
+    public String getBeschreibung(){
+        return this.beschreibung;
+    }
+    @Override
+    public Effekt getEffekt(){
+        return this;
+    }
+
+  
+}
